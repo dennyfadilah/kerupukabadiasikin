7d28ced9a34ca50e1348e51a88f6b43322ee3363
Denny Fadilah <42080971+dennyfadilah@users.noreply.github.com>
2024-05-23 16:26:26 +0700

    Update HomeController.php
    Edit bagian MTH ->MTG

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -7,14 +7,14 @@ function home()
     $periode = date('Y-m-d');
 
     $resultMTH = getPersediaanMTH($periode);
-    $resultMTG = getPersediaanMTH($periode);
+    $resultMTG = getPersediaanMTG($periode);
 
 
     $getDataMTH = getPenjualanMTH($periode);
     $mth = generateDataMTH($getDataMTH);
 
     $getDataMTG = getPenjualanMTG($periode);
-    $mtg = generateDataMTH($getDataMTG);
+    $mtg = generateDataMTG($getDataMTG);
 
     require_once "views/home.php";
 }
